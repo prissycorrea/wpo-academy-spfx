715ce15433b2de265c6358490efc058fbaa997e7
prissycorrea <pris.pcd@gmail.com>
2025-07-01 16:28:28 -0300

    new project config

---
+++ src/webparts/treinamentoSpFx/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITreinamentoSpFxWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TreinamentoSpFxWebPartStrings' {
+  const strings: ITreinamentoSpFxWebPartStrings;
+  export = strings;
+}
